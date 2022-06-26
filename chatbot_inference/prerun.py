0b5ecc26a3ee4e7da8e72cac82f14571dc770e3a
Omri Bitton <omribitt@Omris-MacBook-Pro.local>
2022-06-26 15:40:39 +0300

    added check for model coming from training

--- chatbot_inference/prerun.py
+++ chatbot_inference/prerun.py
@@ -5,7 +5,7 @@ import pathlib
 
 def download_model_files():
     current_dir = pathlib.Path(__file__).parent.resolve()
-    if not os.path.exists(str(current_dir) + '/chatbot_model.pt'):
+    if not os.path.exists(str(current_dir) + '/chatbot_model.pt') and not os.path.exists('input/train/chatbot_model.pt'):
         MODEL_FILE_URL = "https://blueprints-data.s3.us-west-2.amazonaws.com/model_files/chatbot/chatbot_model.pt"
         print('Downloading Model Files...')
         response = requests.get(MODEL_FILE_URL)
